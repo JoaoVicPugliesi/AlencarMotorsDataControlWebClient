e69880c15ee80fad08f8c992076261767806905f
Joao Pugliesi <mrlanguages62@gmail.com>
2026-09-15 14:24:41 -0300

    fix open_close_diary

--- js/useCases/diary/open_close_diary.js
+++ js/useCases/diary/open_close_diary.js
@@ -2,6 +2,7 @@ import diary_component from "../../components/diary/diary_component.js";
 import get_current_date from "../../helpers/get_current_date.js";
 import show_message from "../../helpers/show_message.js";
 import get_stats from "../../infra/use_cases/stat/get_stats.js";
+import post_stats_diary from "../../infra/use_cases/stat/post_stats_diary.js";
 import open_close_diary_customer_portifolios_add from "./open_close_diary_customer_portifolios_add.js";
 import open_close_diary_customer_portifolios_list from "./open_close_diary_customer_portifolios_list.js";
 
