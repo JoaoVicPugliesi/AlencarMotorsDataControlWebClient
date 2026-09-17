71ebbdaf4f989ae549fe1ac6ef5e91947e3543b8
Joao Pugliesi <mrlanguages62@gmail.com>
2026-09-17 14:39:01 -0300

    change base_url value

--- js/infra/base_URL.js
+++ js/infra/base_URL.js
@@ -1,5 +1,5 @@
-const url = 'http://127.0.0.1:3000';
-// const url = 'https://api.alencarmotorsdatacontrol.com'
+// const url = 'http://127.0.0.1:3000';
+const url = 'https://api.alencarmotorsdatacontrol.com'
 const base_URL = url;
 
 export default base_URL;
